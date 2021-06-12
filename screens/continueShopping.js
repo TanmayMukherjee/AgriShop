e8d1a3fcf01fccb9d7fd378dc5eeaa01b92531b3
TanmayMukherjee <67747814+TanmayMukherjee@users.noreply.github.com>
2021-06-12 17:23:54 +0530

    Add files via upload

---
+++ screens/continueShopping.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Modal, KeyboardAvoidingView, ScrollView } from 'react-native';
+import firebase from 'firebase';
+
+export default class ContinueShoppingScreen extends React.Component {
+render(){
+    return(
+        <View></View>
+    )
+}
+
+}
---
+++ screens/placeOrder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Modal, KeyboardAvoidingView, ScrollView } from 'react-native';
+import firebase from 'firebase';
+
+export default class PlaceOrderScreen extends React.Component {
+render(){
+    return(
+        <View>
+            <Text>HELLO PLACE ORDER PLS</Text>
+        </View>
+    )
+}
+
+}
